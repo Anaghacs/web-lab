4552a8c236da35b7a7de0737bb1f9dbf8ee05842
Anaghacs <93421971+Anaghacs@users.noreply.github.com>
2022-03-03 13:57:45 +0530

    php

---
+++ php/arrsort.php
@@ -0,0 +1,34 @@
+<html>
+<body>
+<?php
+$arr=array(20,15,8,1,14,2);
+echo "Array before sorting";
+foreach($arr as $i)
+{
+	echo "$i";
+}
+echo "<br> Array after sorting";
+ArraySort($arr);
+funtion ArraySort($arr)
+{
+	$n=count($arr);
+	for($i=0; $i<$n-1; $i++)
+	{
+		for($j=$i+1; $j<$n; $j++)
+		{
+			if($arr[$i]>$arr[$j])
+			{
+				$temp=$arr[$i];
+				$arr[$i]=$arr[$j];
+				$arr[$j]=$temp;
+			}
+		}
+	}
+	foreach($arr as $i)
+	{
+		echo "$i";
+	}
+}
+?>
+</body>
+</html>
---
+++ php/factorial.php
@@ -0,0 +1,5 @@
+<html>
+<body>
+<label>Enter your number:</label>
+<input type="text" name="number"/>
+<button type="submit" name="submit"><br><continue
